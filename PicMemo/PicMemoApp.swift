c56c20468355e22673bdd28b75b35506db50e326
Yifeng Sun <yifengsun@yifengsun.com>
2023-11-07 16:26:12 -0500

    Initial Commit

---
+++ PicMemo/PicMemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  PicMemoApp.swift
+//  PicMemo
+//
+//  Created by Yifeng Sun on 11/7/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PicMemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
